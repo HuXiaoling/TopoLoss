d7810ad5b0a25e374667e76e37cfef19d22c0214
Xiaoling Hu <1392244065@qq.com>
2020-07-02 10:52:15 -0400

    Update compile_pers_lib.sh

--- Code/cPers/cPers/compile_pers_lib.sh
+++ Code/cPers/cPers/compile_pers_lib.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-# Setup: use `/usr/local/Cellar/python/2.7.13/bin/python-config`
 g++ -O3 -w -shared -std=c++11 -I ../ -I pybind11-stable/include `python3.6-config --cflags --ldflags --libs` PersistencePython.cpp Debugging.cpp PersistenceIO.cpp -o ../../TDFPython/PersistencePython.so
