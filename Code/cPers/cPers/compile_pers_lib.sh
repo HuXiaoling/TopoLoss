423dbc4a7d0d0063eda98e36f5c19c44165653b8
Xiaoling Hu <1392244065@qq.com>
2020-07-02 10:53:28 -0400

    Update compile_pers_lib.sh

--- Code/cPers/cPers/compile_pers_lib.sh
+++ Code/cPers/cPers/compile_pers_lib.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+# python2 and python3 should both work, depends on your own environment.
 g++ -O3 -w -shared -std=c++11 -I ../ -I pybind11-stable/include `python3.6-config --cflags --ldflags --libs` PersistencePython.cpp Debugging.cpp PersistenceIO.cpp -o ../../TDFPython/PersistencePython.so
